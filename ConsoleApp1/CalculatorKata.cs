f2dfd0d7237432548993b299ed25909d03185000
Andy Collins <andymcollins@gmail.com>
2017-08-17 11:21:00 +0100

    Add files via upload

---
+++ ConsoleApp1/CalculatorKata.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorKata
+{
+
+   
+    public class Calculator
+    {
+
+        static void Main(string[] args)
+        {
+        }
+        public string[] GetDelimiters(string text)
+        {
+            var delimiters = new List<string> { ",", "\n" };
+
+            if (text.StartsWith("//"))
+            {
+                string[] customSeperators = { "[", "]" };
+                var customSeperator = text.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries).First();
+
+                text = text.Substring(customSeperator.Length, text.Length - customSeperator.Length);
+                var allCustomSeperators = customSeperator.Split(customSeperators, StringSplitOptions.RemoveEmptyEntries);
+              
+                foreach (var sep in allCustomSeperators)
+                {
+                    delimiters.Add(sep);
+                }
+
+                delimiters.Add(text.Substring(2, 1));
+            }
+            return delimiters.ToArray();
+        }
+
+        public int Add(string numbers)
+        {
+            var numberArray = numbers.Split(GetDelimiters(numbers), StringSplitOptions.None);
+            string negativeNumbers = "";
+            
+            int totalsum = 0;
+            foreach (string number in numberArray)
+            {
+                int.TryParse(number, out int result);
+                if (result < 0) negativeNumbers += number + " ";
+                
+                if (result <1000) totalsum += result;
+            }
+            if (negativeNumbers.Length <= 0) return totalsum;
+
+            Console.WriteLine(negativeNumbers);
+            throw new Exception($"negatives not allowed! you entered: {negativeNumbers}");
+        }
+    }
+}
---
+++ tests/CalculatorTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+
+
+
+namespace tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private CalculatorKata.Calculator _calculator;
+
+        [SetUp]
+        public void InitFixture() => _calculator = new CalculatorKata.Calculator();
+
+        [Test]
+        public void GetResultWhenAddingWithEmptyString()
+        {
+            Assert.That(_calculator.Add(string.Empty), Is.EqualTo(0));
+        }
+
+        [TestCase("1", 1, TestName = "Test One Number"), 
+         TestCase("100,2", 102, TestName = "Test with Two Numbers"),
+         TestCase("100,10,1,2,3", 116, TestName = "Test with Multiple Numbers"), 
+         TestCase("1\n2", 3, TestName = "Test With NewLine Delimiter"),
+         TestCase("100\n10,1", 111, TestName = "Test with Mixed Delimiters"),
+         TestCase("//;\n1;2", 3, TestName = "Test with Custom Delimiter"),
+         TestCase("1001,1", 1, TestName = "Test with Number Greater than 1000"),
+         TestCase("//[***]\n1***2***3", 6, TestName = "Test with Custom Delimiter any Length"),
+         TestCase("//[*][%]\n1*2%3", 6, TestName = "Test with Multiple Custom Delimiter any Length")
+        ]
+        public void GetResultFromTestCase(string number, int expectedResult)
+        {
+            Assert.That(_calculator.Add(number), Is.EqualTo(expectedResult));
+        }
+
+
+        [Test]
+        public void GetExceptionWhenUsingNegativeNumber()
+        {
+            var calculator = new CalculatorKata.Calculator();
+
+            var ex = Assert.Throws<Exception>(() => calculator.Add("-1"));
+
+            Assert.That(ex.Message, Is.EqualTo("negatives not allowed! you entered: -1 "));
+        }
+
+        [Test]
+        public void GetExceptionWhenUsingMultipleNegativeNumbers()
+        {
+            var calculator = new CalculatorKata.Calculator();
+
+            var ex = Assert.Throws<Exception>(() => calculator.Add("-1,-2"));
+
+            Assert.That(ex.Message, Is.EqualTo("negatives not allowed! you entered: -1 -2 "));
+        }
+    }
+}
